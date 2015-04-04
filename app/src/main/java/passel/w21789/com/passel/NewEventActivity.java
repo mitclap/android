ed2805050ae06161c754c2d6df0312091a6197b9
Priscila Cortez <priscila@mit.edu>
2015-04-04 18:50:40 -0400

    Opens Calendar App of your choice

--- app/src/main/java/passel/w21789/com/passel/NewEventActivity.java
+++ app/src/main/java/passel/w21789/com/passel/NewEventActivity.java
@@ -2,14 +2,26 @@ package passel.w21789.com.passel;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.CalendarView;
+
+import android.database.Cursor;
+import android.provider.CalendarContract;
+import android.text.format.DateFormat;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 
 public class NewEventActivity extends ActionBarActivity {
@@ -21,10 +33,23 @@ public class NewEventActivity extends ActionBarActivity {
 
     private SimpleDateFormat dateFormatter;
 
+    private CalendarView calendarView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event);
+
+        // Opens Calendar app
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(new Date());
+        cal.add(Calendar.MONTH, 2);
+        long time = cal.getTime().getTime();
+        Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+        builder.appendPath("time");
+        builder.appendPath(Long.toString(time));
+        Intent intent = new Intent(Intent.ACTION_VIEW, builder.build());
+        startActivity(intent);
     }
 
     private boolean createEvent(){
@@ -43,7 +68,10 @@ public class NewEventActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
+
+        int id = item.getItemId();
+
+        switch (id) {
             case R.id.new_event_cancel:
                 finish();
                 return true;
