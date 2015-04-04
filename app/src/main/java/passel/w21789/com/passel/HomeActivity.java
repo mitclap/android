f5b5601607c0233e57467606593de672c8a12f2f
Lisandro Jimenez <jimenezl@mit.edu>
2015-04-04 16:24:56 -0400

    got map to show up

--- app/src/main/java/passel/w21789/com/passel/HomeActivity.java
+++ app/src/main/java/passel/w21789/com/passel/HomeActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class HomeActivity extends ActionBarActivity {
@@ -13,6 +15,19 @@ public class HomeActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        addMapButtonListener();
+    }
+
+    private void addMapButtonListener(){
+        Button mapButton = (Button) findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomeActivity.this, MapEventActivity.class);
+//        myIntent.putExtra("key", value); //Optional parameters
+                HomeActivity.this.startActivity(myIntent);
+            }
+        });
     }
 
 
@@ -21,6 +36,8 @@ public class HomeActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
+
+
     }
 
     @Override
