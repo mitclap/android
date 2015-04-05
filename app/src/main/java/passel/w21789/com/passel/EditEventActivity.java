714beef024d63a012fc1cd9b339fdbf15ee4660b
Carlos Henriquez <carlosh@mit.edu>
2015-04-04 20:25:14 -0400

    Add skeleton code for EditEventActivity

---
+++ app/src/main/java/passel/w21789/com/passel/EditEventActivity.java
@@ -0,0 +1,90 @@
+package passel.w21789.com.passel;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.CalendarView;
+
+import android.database.Cursor;
+import android.provider.CalendarContract;
+import android.text.format.DateFormat;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/** Temporarily identical to the NewEventActivity
+ * TODO: Discuss representation of events that will populate home screen
+ * TODO: Change this so that it actually edits an event
+ */
+public class EditEventActivity extends Activity {
+    private EditText fromDateEtxt;
+    private EditText toDateEtxt;
+
+    private DatePickerDialog fromDatePickerDialog;
+    private DatePickerDialog toDatePickerDialog;
+
+    private SimpleDateFormat dateFormatter;
+
+    private CalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_event);
+
+        // Opens Calendar app
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(new Date());
+        cal.add(Calendar.MONTH, 2);
+        long time = cal.getTime().getTime();
+        Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+        builder.appendPath("time");
+        builder.appendPath(Long.toString(time));
+        Intent intent = new Intent(Intent.ACTION_VIEW, builder.build());
+        startActivity(intent);
+    }
+
+    private boolean createEvent(){
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_event, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.new_event_cancel:
+                finish();
+                return true;
+            case R.id.new_event_check:
+                if (createEvent()){
+                    finish();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
