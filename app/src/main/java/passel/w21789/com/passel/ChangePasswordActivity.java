5a42ea67f9d72fa96b43f3e2fabd00038dadbbc2
Carlos Henriquez <carlosh@mit.edu>
2015-04-05 02:09:17 -0400

    Add small validator for password changer

--- app/src/main/java/passel/w21789/com/passel/ChangePasswordActivity.java
+++ app/src/main/java/passel/w21789/com/passel/ChangePasswordActivity.java
@@ -1,5 +1,6 @@
 package passel.w21789.com.passel;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -7,6 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class ChangePasswordActivity extends ActionBarActivity {
@@ -22,8 +25,19 @@ public class ChangePasswordActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_change_password, menu);
         return true;
+    }
 
-
+    private boolean checkPasswordAndConfirmPassword(String password,String confirmPassword)
+    {
+        boolean confirmed = false;
+        if (confirmPassword != null && password != null)
+        {
+            if (password.equals(confirmPassword))
+            {
+                confirmed = true;
+            }
+        }
+        return confirmed;
     }
 
     @Override
@@ -42,10 +56,23 @@ public class ChangePasswordActivity extends ActionBarActivity {
                 // Go back to the settings page
                 //TODO: Insert password changing logic here
                 //TODO: Make it display that little text at the bottom if the pw was changed
-                Intent myIntent = new Intent(ChangePasswordActivity.this, SettingsActivity.class);
-//        myIntent.putExtra("key", value); //Optional parameters
-                ChangePasswordActivity.this.startActivity(myIntent);
-                return true;
+                //Take inputs from the password fields
+                String password = ((EditText)findViewById(R.id.passwordNew)).getText().toString();
+                String passwordConfirm = ((EditText)findViewById(R.id.passwordConfirm)).getText().toString();
+                boolean confirmed = checkPasswordAndConfirmPassword(password, passwordConfirm);
+                if (confirmed) {
+                    finish();
+                    return true;
+                } else {
+                    Context context = getApplicationContext();
+                    String text = "Passwords do not match";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    
+                }
+
             default:
                 return super.onOptionsItemSelected(item);
         }
