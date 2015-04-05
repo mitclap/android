f3fc2b4c5871bf5789618844ce2f04982690513b
Aneesh Agrawal <aneeshusa@gmail.com>
2015-04-05 18:36:30 -0400

    Add Constants class.

---
+++ app/src/main/java/passel/w21789/com/passel/Constants.java
@@ -0,0 +1,10 @@
+package passel.w21789.com.passel;
+
+import java.net.URL;
+
+/**
+ * Created by aneesh on 4/5/15.
+ */
+public class Constants {
+    public static final String API_BASE_URL = "http://18.111.3.60:5000";
+}
