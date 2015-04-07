be762bd42b274b6c67d60b02925177ea8f14b755
Priscila Cortez <priscila@mit.edu>
2015-04-07 04:35:21 -0400

    add multiple guests

--- app/src/main/java/passel/w21789/com/passel/NewEventActivity.java
+++ app/src/main/java/passel/w21789/com/passel/NewEventActivity.java
@@ -2,19 +2,26 @@ package passel.w21789.com.passel;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -31,20 +38,28 @@ public class NewEventActivity extends ActionBarActivity{
     private DatePickerDialog dpDialog;
     private TimePickerDialog tpDialog;
 
-    // Later to be used for autocomplete
-    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
-            new LatLng(-85, 180), new LatLng(85, -180)
-    );
+    private EditText guestInput;
+    private Button addGuestButton;
+    private ListView guestList;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> guestNameList=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event);
 
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                guestNameList);
         fromDateButton = (ImageButton) findViewById(R.id.start_date_button);
         toDateButton = (ImageButton) findViewById(R.id.end_date_button);
         fromTimeEtxt = (TextView) findViewById(R.id.start_time_data);
         toTimeEtxt = (TextView) findViewById(R.id.end_time_data);
+        guestInput = (EditText) findViewById(R.id.guest_input);
+        addGuestButton = (Button) findViewById(R.id.add_guest_button);
+        guestList = (ListView) findViewById(R.id.guest_list_view);
+        guestList.setAdapter(adapter);
 
         // Setting Date
         fromDateButton.setOnClickListener(new OnClickListener(){
@@ -72,6 +87,31 @@ public class NewEventActivity extends ActionBarActivity{
             @Override
             public void onClick(View v) {
                 setTime(R.id.end_time_data);
+            }
+        });
+
+        //Listening to see if a user pressed enter if they are entering a Guest
+        guestInput.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER){
+                    addGuestButton.callOnClick();
+                }
+                return false;
+            }
+        });
+
+        addGuestButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("add guest");
+                String guestInputText = String.valueOf(guestInput.getText());
+                if (!guestInputText.equals("") && !guestInputText.equals(" ")){
+                    guestNameList.add(guestInputText);
+                }
+                guestInput.setText("");
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(guestInput.getWindowToken(),0);
             }
         });
     }
