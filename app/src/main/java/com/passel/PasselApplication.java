585569eec303f5bbcd21b426eddbadf7c2f5461b
Aneesh Agrawal <aneeshusa@gmail.com>
2015-04-19 22:23:23 -0400

    Use own Application subclass to centralize persistence.

---
+++ app/src/main/java/com/passel/PasselApplication.java
@@ -0,0 +1,44 @@
+package com.passel;
+
+import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
+
+/**
+ * Created by aneesh on 4/19/15.
+ */
+public class PasselApplication extends Application {
+    public PasselApplication() {
+        super();
+    }
+
+    /**
+     * Called when the application is starting, before any activity, service,
+     * or receiver objects (excluding content providers) have been created.
+     * Implementations should be as quick as possible (for example using
+     * lazy initialization of state) since the time spent in this function
+     * directly impacts the performance of starting the first activity,
+     * service, or receiver in a process.
+     * If you override this method, be sure to call super.onCreate().
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+    }
+
+}
