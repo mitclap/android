9861203522cbd579ac0e0033d71ed3876033cfad
Aneesh Agrawal <aneeshusa@gmail.com>
2015-04-19 21:15:36 -0400

    Clean up login and signup screens to prep for functionality infusion.

--- app/src/main/java/com/passel/LogInActivity.java
+++ app/src/main/java/com/passel/LogInActivity.java
@@ -1,12 +1,9 @@
 package com.passel;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.passel.R;
 
 
 public class LogInActivity extends Activity {
@@ -20,20 +17,13 @@ public class LogInActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_log_in, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (R.id.action_settings == item.getItemId()) {
             return true;
         }
 
