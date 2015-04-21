06a81d374f93e3962d1212d27c4e78f07700d78a
Lisandro Jimenez <jimenezl@mit.edu>
2015-04-21 00:34:31 -0400

    sign up creates a new user on the server

--- app/src/main/java/com/passel/SignUpActivity.java
+++ app/src/main/java/com/passel/SignUpActivity.java
@@ -3,9 +3,18 @@ package com.passel;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.passel.api.APIClient;
+import com.passel.api.APIError;
+import com.passel.api.APIResponse;
+import com.passel.data.JsonMapper;
+import com.passel.util.Result;
 
 public class SignUpActivity extends Activity {
 
@@ -16,10 +25,47 @@ public class SignUpActivity extends Activity {
         findViewById(R.id.sign_up_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SignUpActivity.this.startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
+                onSignUpClick();
             }
         });
         findViewById(R.id.sign_up_username).requestFocus();
+    }
+
+    private void onSignUpClick() {
+        String usernameInput = ((EditText) findViewById(R.id.sign_up_username)).getText().toString();
+
+        String passwordInput = ((EditText) findViewById(R.id.sign_up_password)).getText().toString();
+        String confirmPasswordInput = ((EditText) findViewById(R.id.sign_up_confirm_password)).getText().toString();
+
+        if (!passwordInput.equals(confirmPasswordInput)) {
+            showToast("Passwords do not match");
+            return;
+        }
+
+        if (!usernameInput.matches("^[a-zA-Z0-9]+$")) {
+            showToast("Invalid username: only lowercase/uppercase letters and numbers");
+            return;
+        }
+
+        JsonMapper mapper = ((PasselApplication) getApplication()).getJsonMapper();
+        Result<APIResponse, APIError> result = new APIClient(mapper).signup(usernameInput, passwordInput);
+
+        if (result.isOk()) {
+            if (result.unwrap().getCode() == 409) {
+                showToast("Username already exists");
+                return;
+            }
+            SignUpActivity.this.startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
+        } else {
+            showToast("Unable to communicate to server");
+            return;
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_LONG).show();
+        return;
     }
 
     @Override
