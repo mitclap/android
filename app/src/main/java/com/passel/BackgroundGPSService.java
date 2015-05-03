b8771aaa7529b11171c8db1a49fbd94b968b9cd6
Lisandro Jimenez <jimenezl@mit.edu>
2015-05-02 20:13:37 -0400

    make GPS positioning more accurate

--- app/src/main/java/com/passel/BackgroundGPSService.java
+++ app/src/main/java/com/passel/BackgroundGPSService.java
@@ -14,11 +14,12 @@ import com.passel.data.Location;
 
 public class BackgroundGPSService extends Service {
 
-    private final int REQUEST_LOCATION_UPDATE_TIMER = 30*1000;
+    private final int REQUEST_LOCATION_UPDATE_TIMER = 5*1000;
     private final int REQUEST_LOCATION_UPDATE_MINDISTANCE_METER = 0;
     LocationManager locationManager;
     LocationManager locationManagerGPS;
     LocationListener locationListener;
+    android.location.Location oldLocation;
 
     private void sendMessage(android.location.Location location) {
         Log.d("sender", "Broadcasting message");
@@ -57,15 +58,82 @@ public class BackgroundGPSService extends Service {
                 REQUEST_LOCATION_UPDATE_MINDISTANCE_METER, // 500
                 locationListener);
 
-        android.location.Location lastLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        if (lastLocation == null){
-            lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        android.location.Location lastLocationNetwork = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        android.location.Location lastLocationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        android.location.Location bestLastLocation;
+
+        if (isBetterLocation(lastLocationNetwork, lastLocationGPS)) {
+            bestLastLocation = lastLocationNetwork;
+        } else {
+            bestLastLocation = lastLocationGPS;
         }
-        if (lastLocation != null){
-            sendMessage(lastLocation);
+
+        if (isBetterLocation(bestLastLocation, oldLocation)) {
+            oldLocation = bestLastLocation;
+            sendMessage(oldLocation);
+        } else {
+            sendMessage(oldLocation);
         }
 
         return START_NOT_STICKY;
+    }
+
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+     */
+    protected boolean isBetterLocation(android.location.Location location, android.location.Location currentBestLocation) {
+        final int FIVE_SECONDS = 1000 * 5;
+
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > FIVE_SECONDS;
+        boolean isSignificantlyOlder = timeDelta < -FIVE_SECONDS;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
     }
 
     @Override
